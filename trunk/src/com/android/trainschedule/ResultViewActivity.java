4dac166abad42277e298bcf8cc8ed79ac64ac81c
aselalee <aselalee@93d08951-fc07-b37f-cc7d-7371ff8aefea>
2011-10-25 07:07:52 +0000

    Added comments and cleaned up some code.

--- trunk/src/com/android/trainschedule/ResultViewActivity.java
+++ trunk/src/com/android/trainschedule/ResultViewActivity.java
@@ -1,16 +1,11 @@
 package com.android.trainschedule;
 
-
-
 import android.app.Activity;
 import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
-import android.view.KeyEvent;
 import android.webkit.WebView;
-import android.webkit.WebViewClient;
-
 
 public class ResultViewActivity extends Activity implements Runnable {
 	String station_from;
@@ -27,51 +22,59 @@ public class ResultViewActivity extends Activity implements Runnable {
 	public void onCreate(Bundle savedInstanceState) {
 	    super.onCreate(savedInstanceState);
 	    setContentView(R.layout.result_table);
-	
-	    Bundle extras = getIntent().getExtras(); 
-	    if(extras !=null) {
-	    	station_from = extras.getString("station_from");
-	    	station_to = extras.getString("station_to");
-	    	time_from = extras.getString("time_from");
-	    	time_to = extras.getString("time_to");
-	    }
+
+	    	/**
+	    	 * Read data passed from the calling activity.
+	    	 */
+	    	Bundle extras = getIntent().getExtras(); 
+	    	if(extras !=null) {
+	    		station_from = extras.getString("station_from");
+	    		station_to = extras.getString("station_to");
+	    		time_from = extras.getString("time_from");
+	    		time_to = extras.getString("time_to");
+	    	}
+
+	    	/**
+	    	 * Get the webview handle.
+	    	 */
+	    	mWebView = (WebView) findViewById(R.id.webview);
 	    
-	    mWebView = (WebView) findViewById(R.id.webview);
-	    mWebView.getSettings().setJavaScriptEnabled(true);
-	    
-		pd = ProgressDialog.show(this, "Working..",
-								 "Getting Results from www.railway.gov.lk",
-								 true, false);
-		Thread thread = new Thread(this);
-		thread.start();   
+	    	/**
+	    	 * Display progress Dialog.
+	    	 */
+	    	pd = ProgressDialog.show(this, "Working..",
+								 	"Getting Results from www.railway.gov.lk",
+								 	true, false);
+
+	    	/**
+	    	 * This will execute the "run" method in a new thread.
+	    	 */
+	    	Thread thread = new Thread(this);
+	    	thread.start();
 	}
-	private class myWebViewClient extends WebViewClient {
-	    @Override
-	    public boolean shouldOverrideUrlLoading(WebView view, String url) {
-	        view.loadUrl(url);
-	        return true;
-	    }
-	}
-	@Override
-	public boolean onKeyDown(int keyCode, KeyEvent event) {
-	    if ((keyCode == KeyEvent.KEYCODE_BACK) && mWebView.canGoBack()) {
-	        mWebView.goBack();
-	        return true;
-	    }
-	    return super.onKeyDown(keyCode, event);
-	}
-	
+	/**
+	 * run() method that must be implemented when implementing "Runnable" class.
+	 */
 	public void run() {
+		/**
+		 * Call the "GetResults" method to retrieve data from server.
+		 */
 		result= GetResultsFromSite.GetResults(station_from, station_to, time_from, time_to);
+		/**
+		 * This will send message to the calling thread to continue and display data.
+		 */
 		handler.sendEmptyMessage(0);
 	}
-	
+	/**
+	 * Handler variable which is used to handle processing after results are received.
+	 * 1. Remove the progress dialog
+	 * 2. Display the resultant HTML
+	 */
 	private Handler handler = new Handler() {
 		@Override
 		public void handleMessage(Message msg) {
 			pd.dismiss();
 		    mWebView.loadDataWithBaseURL("", result,"text/html", "UTF-8", null);
-		    mWebView.setWebViewClient(new myWebViewClient());
 		}
 	};
 }
