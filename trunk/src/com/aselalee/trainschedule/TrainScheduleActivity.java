433344bbb9601bed2c4af35e951995cfa68ecbf7
aselalee <aselalee@93d08951-fc07-b37f-cc7d-7371ff8aefea>
2011-12-05 09:47:35 +0000

    Added the functionality to add parameters to favourites when in results view.

--- trunk/src/com/aselalee/trainschedule/TrainScheduleActivity.java
+++ trunk/src/com/aselalee/trainschedule/TrainScheduleActivity.java
@@ -379,6 +379,9 @@ public class TrainScheduleActivity extends Activity implements Runnable {
 		}
 	};
     private void getNewFavName() {
+    	if( validateStations() == false) {
+    		return;
+    	}
         LayoutInflater factory = LayoutInflater.from(this);
         View textEntryView = factory.inflate(R.layout.text_entry_dialog, null);
         final EditText et = (EditText)textEntryView.findViewById(R.id.new_name);
@@ -451,7 +454,7 @@ public class TrainScheduleActivity extends Activity implements Runnable {
         		return true;
         	}
        	Toast.makeText(this,
-                        "Check Station Names", Toast.LENGTH_LONG).show();
+                        "Invalid Station Names", Toast.LENGTH_LONG).show();
         return false;
     }
     private int searchString(String strArray[], String strSrc) {
@@ -469,9 +472,13 @@ public class TrainScheduleActivity extends Activity implements Runnable {
     private void populateIntent(Intent intent) {
     	String date_today = android.text.format.DateFormat.format("yyyy-MM-dd", new java.util.Date()).toString();
     	intent.putExtra("station_from", station_from_val);
+    	intent.putExtra("station_from_txt", station_from_txt);
     	intent.putExtra("station_to", station_to_val);
+    	intent.putExtra("station_to_txt", station_to_txt);
     	intent.putExtra("time_from", time_from_val);
+    	intent.putExtra("time_from_txt", time_from_txt);
     	intent.putExtra("time_to", time_to_val);
+    	intent.putExtra("time_to_txt", time_to_txt);
     	intent.putExtra("date_today", date_today);
     }
 }
