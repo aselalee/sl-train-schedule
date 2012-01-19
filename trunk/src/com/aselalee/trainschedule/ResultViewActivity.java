940d3891bc59b204a638aba7f26b3a93ec1f3e43
aselalee <aselalee@93d08951-fc07-b37f-cc7d-7371ff8aefea>
2012-01-19 10:07:31 +0000

    Fixed a bug where progress dialog in results view is canceled when clicked outside of dialog bounds (only in 3.0 and upwards). When calling dismiss on a canceled dialog box the application crashed. Spinner style and spinner drop down style changed.

--- trunk/src/com/aselalee/trainschedule/ResultViewActivity.java
+++ trunk/src/com/aselalee/trainschedule/ResultViewActivity.java
@@ -115,6 +115,7 @@ public class ResultViewActivity extends Activity implements Runnable {
 						finish();
 					}
 				});
+		pd.setCanceledOnTouchOutside(false);
 
 		/**
 		 * This will execute the "run" method in a new thread.
@@ -151,7 +152,9 @@ public class ResultViewActivity extends Activity implements Runnable {
 		@Override
 		public void handleMessage(Message msg) {
 			if(msg.arg1 == Constants.THREAD_GET_RESULTS) {
-				pd.dismiss();
+				if(pd.isShowing() == true) {
+					pd.dismiss();
+				}
 				if(isStop == false) {
 					if(results != null) {
 						isAddToFavsActive = true;
