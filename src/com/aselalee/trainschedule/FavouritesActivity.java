0af729d32a47535b805037ff93f5bdd9c4d55473
aselalee <aselalee@93d08951-fc07-b37f-cc7d-7371ff8aefea>
2012-01-26 12:19:01 +0000

    updated to android standard dialog box handling in FavouritesActivity.java

--- src/com/aselalee/trainschedule/FavouritesActivity.java
+++ src/com/aselalee/trainschedule/FavouritesActivity.java
@@ -18,7 +18,9 @@
 package com.aselalee.trainschedule;
 
 import android.app.AlertDialog;
+import android.app.Dialog;
 import android.app.ListActivity;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.res.Configuration;
@@ -34,6 +36,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.AdapterContextMenuInfo;
 import android.widget.AdapterView.OnItemClickListener;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
@@ -41,6 +44,9 @@ import android.widget.Toast;
 public class FavouritesActivity extends ListActivity {
 	private ParameterSet [] paramsList = null;
 	private HisAndFavAdapter adapter = null;
+	private int postionToRename = 0;
+
+	private static final int DIALOG_RENAME_FAV = 5;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -57,7 +63,7 @@ public class FavouritesActivity extends ListActivity {
 				}
 			}
 		});
-		adapter = new HisAndFavAdapter(this, paramsList, true);
+		adapter = new HisAndFavAdapter(FavouritesActivity.this, paramsList, false);
 		setListAdapter(adapter);
 	}
 
@@ -74,7 +80,7 @@ public class FavouritesActivity extends ListActivity {
 		 * 2. Then show it in the list view.
 		 * 3. Call this when ever the database is updated.
 		 */
-		DBDataAccess myDBAcc = new DBDataAccess(this);
+		DBDataAccess myDBAcc = new DBDataAccess(FavouritesActivity.this);
 		paramsList = myDBAcc.GetFavourites();
 		adapter.paramSet = paramsList;
 		myDBAcc.close();
@@ -98,7 +104,7 @@ public class FavouritesActivity extends ListActivity {
 	public boolean onOptionsItemSelected(MenuItem item) {
 		switch (item.getItemId()) {
 			case R.id.fav_menu_clear_fav:
-				DBDataAccess myDBAcc = new DBDataAccess(this);
+				DBDataAccess myDBAcc = new DBDataAccess(FavouritesActivity.this);
 				myDBAcc.ClearFavouritesTable();
 				myDBAcc.close();
 				onResume();
@@ -121,26 +127,49 @@ public class FavouritesActivity extends ListActivity {
 	@Override
 	public boolean onContextItemSelected(MenuItem item) {
 		AdapterContextMenuInfo info = (AdapterContextMenuInfo) item.getMenuInfo();
-		DBDataAccess myDBAcc = null;
 		switch (item.getItemId()) {
 			case R.id.fav_ctx_menu_rename_fav:
-				myDBAcc = new DBDataAccess(this);
-				renameFavItem(info.position, myDBAcc);
+				postionToRename = info.position;
+				showDialog(DIALOG_RENAME_FAV);
 				break;
 			case R.id.fav_ctx_menu_delete_fav:
-				myDBAcc = new DBDataAccess(this);
+				DBDataAccess myDBAcc = new DBDataAccess(FavouritesActivity.this);
 				myDBAcc.DeleteFavRecord(paramsList[info.position].id);
+				myDBAcc.close();
 				break;
 			default:
 				return super.onContextItemSelected(item);
 		}
-		myDBAcc.close();
+		
 		onResume();
 		return true;
 	}
 
+	protected Dialog onCreateDialog(int id) {
+		Dialog dialog = null;
+		switch(id) {
+			case DIALOG_RENAME_FAV:
+				dialog = renameFavItem();
+				break;
+			default:
+				dialog = null;
+		}
+		return dialog;
+	}
+	protected void onPrepareDialog(int id, Dialog dialog) {
+		switch(id) {
+			case DIALOG_RENAME_FAV:
+				EditText et = (EditText)dialog.findViewById(R.id.dialog_new_name);
+				et.setText(paramsList[postionToRename].name);
+				break;
+			default:
+				return;
+		}
+		return;
+	}
+
 	private void getResults(ParameterSet paramSet) {
-		Intent intent = CommonUtilities.GetResultViewIntent(this);
+		Intent intent = CommonUtilities.GetResultViewIntent(FavouritesActivity.this);
 		CommonUtilities.PupulateIntentForResultsActivity(
 				paramSet.start_station_val, paramSet.start_station_txt,
 				paramSet.end_station_val, paramSet.end_station_txt,
@@ -150,21 +179,26 @@ public class FavouritesActivity extends ListActivity {
 		startActivity(intent);
 	}
 
-	private void renameFavItem(final int itemPosition, final DBDataAccess myDBAcc) {
-		LayoutInflater factory = LayoutInflater.from(this);
+	private Dialog renameFavItem() {
+		final Context thisContext = FavouritesActivity.this;
+		LayoutInflater factory = LayoutInflater.from(FavouritesActivity.this);
 		View textEntryView = factory.inflate(R.layout.text_entry_dialog, null);
 		final EditText et = (EditText)textEntryView.findViewById(R.id.dialog_new_name);
-		et.setText(paramsList[itemPosition].name);
-		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+		et.setText(paramsList[postionToRename].name);
+		final CheckBox cb = (CheckBox)textEntryView.findViewById(R.id.dialog_isTimeFilterOnCB);
+		cb.setVisibility(View.GONE);
+		AlertDialog.Builder builder = new AlertDialog.Builder(FavouritesActivity.this);
 		builder.setView(textEntryView);
 		builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
 			public void onClick(DialogInterface dialog, int id) {
 				String newName = "";
 				newName = et.getEditableText().toString();
 				if(newName.length() != 0) {
-					myDBAcc.RenameFavRecord(paramsList[itemPosition].id, newName);
+					DBDataAccess myDBAcc = new DBDataAccess(thisContext);
+					myDBAcc.RenameFavRecord(paramsList[postionToRename].id, newName);
+					myDBAcc.close();
 				} else {
-					runToast("Invalid name.");
+					Toast.makeText(thisContext, "Invalid name.", Toast.LENGTH_LONG).show();
 				}
 				CommonUtilities.HideSoftKeyboard(et, getBaseContext());
 				onResume();
@@ -178,10 +212,6 @@ public class FavouritesActivity extends ListActivity {
 		});
 		builder.setTitle("Enter New Name");
 		AlertDialog alert = builder.create();
-		alert.show();
-	}
-
-	private void runToast(String msg) {
-		Toast.makeText(this, msg, Toast.LENGTH_LONG).show();
+		return alert;
 	}
 }
