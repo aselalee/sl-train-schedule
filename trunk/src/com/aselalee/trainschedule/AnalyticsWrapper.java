bf5fa767e67c79e26151dd9a4890597f4450e1c0
aselalee <aselalee@93d08951-fc07-b37f-cc7d-7371ff8aefea>
2012-02-15 08:21:38 +0000

    Cosmetic changes to the source.

--- trunk/src/com/aselalee/trainschedule/AnalyticsWrapper.java
+++ trunk/src/com/aselalee/trainschedule/AnalyticsWrapper.java
@@ -1,3 +1,20 @@
+/**
+* @copyright	Copyright (C) 2011 Asela Leelaratne
+* @license		GNU/GPL Version 3
+* 
+* This Application is released to the public under the GNU General Public License.
+* 
+* GNU/GPL V3 Extract.
+* 15. Disclaimer of Warranty.
+* THERE IS NO WARRANTY FOR THE PROGRAM, TO THE EXTENT PERMITTED BY APPLICABLE LAW.
+* EXCEPT WHEN OTHERWISE STATED IN WRITING THE COPYRIGHT HOLDERS AND/OR OTHER PARTIES
+* PROVIDE THE PROGRAM AS IS WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED,
+* INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+* FOR A PARTICULAR PURPOSE. THE ENTIRE RISK AS TO THE QUALITY AND PERFORMANCE OF THE
+* PROGRAM IS WITH YOU. SHOULD THE PROGRAM PROVE DEFECTIVE, YOU ASSUME THE COST OF ALL
+* NECESSARY SERVICING, REPAIR OR CORRECTION.
+*/
+
 package com.aselalee.trainschedule;
 
 import android.content.Context;
@@ -7,7 +24,7 @@ import com.google.android.apps.analytics.GoogleAnalyticsTracker;
 public class AnalyticsWrapper {
 	private boolean useAnalytics = false;
 	private GoogleAnalyticsTracker tracker = null;
-	
+
 	public AnalyticsWrapper(Context mContext) {
 		/**
 		 * useAnalytics can be set to true/false according to a global
