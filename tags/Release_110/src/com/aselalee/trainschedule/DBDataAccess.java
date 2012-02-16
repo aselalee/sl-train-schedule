34e34ae82ba666a3663f812556127ee833940d3a
aselalee <aselalee@93d08951-fc07-b37f-cc7d-7371ff8aefea>
2012-02-16 03:34:48 +0000

    Released to market.

---
+++ tags/Release_110/src/com/aselalee/trainschedule/DBDataAccess.java
@@ -0,0 +1,440 @@
+/**
+* @copyright	Copyright (C) 2011 Asela Leelaratne
+* @license		GNU/GPL Version 3
+* 
+* This Application is released to the public under the GNU General Public License.
+* 
+* GNU/GPL V3 Extract.
+* 15. Disclaimer of Warranty.
+* THERE IS NO WARRANTY FOR THE PROGRAM, TO THE EXTENT PERMITTED BY APPLICABLE LAW.
+* EXCEPT WHEN OTHERWISE STATED IN WRITING THE COPYRIGHT HOLDERS AND/OR OTHER PARTIES
+* PROVIDE THE PROGRAM AS IS WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED,
+* INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+* FOR A PARTICULAR PURPOSE. THE ENTIRE RISK AS TO THE QUALITY AND PERFORMANCE OF THE
+* PROGRAM IS WITH YOU. SHOULD THE PROGRAM PROVE DEFECTIVE, YOU ASSUME THE COST OF ALL
+* NECESSARY SERVICING, REPAIR OR CORRECTION.
+*/
+
+package com.aselalee.trainschedule;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.os.Handler;
+import android.os.Message;
+import android.provider.BaseColumns;
+import android.util.Log;
+
+public class DBDataAccess extends SQLiteOpenHelper {
+
+	public DBDataAccess(Context context) {
+		super(context, Constants.DB_NAME, null, Constants.DB_VER);
+	}
+
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		db.execSQL("CREATE TABLE " + Constants.TABLE_HIS + " ("
+				+ BaseColumns._ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+				+ Constants.COL_START_STATION_TXT + " TEXT,"
+				+ Constants.COL_START_STATION_VAL + " TEXT,"
+				+ Constants.COL_END_STATION_TXT + " TEXT,"
+				+ Constants.COL_END_STATION_VAL + " TEXT,"
+				+ Constants.COL_START_TIME_TXT + " TEXT,"
+				+ Constants.COL_START_TIME_VAL + " TEXT,"
+				+ Constants.COL_END_TIME_TXT + " TEXT,"
+				+ Constants.COL_END_TIME_VAL + " TEXT"
+				+ ");");
+		db.execSQL("CREATE TABLE " + Constants.TABLE_FAV + " ("
+				+ BaseColumns._ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+				+ Constants.COL_START_STATION_TXT + " TEXT,"
+				+ Constants.COL_START_STATION_VAL + " TEXT,"
+				+ Constants.COL_END_STATION_TXT + " TEXT,"
+				+ Constants.COL_END_STATION_VAL + " TEXT,"
+				+ Constants.COL_START_TIME_TXT + " TEXT,"
+				+ Constants.COL_START_TIME_VAL + " TEXT,"
+				+ Constants.COL_END_TIME_TXT + " TEXT,"
+				+ Constants.COL_END_TIME_VAL + " TEXT,"
+				+ Constants.COL_FAV_NAME + " TEXT"
+				+ ");");
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		Log.w(Constants.LOG_TAG, "Upgrading database from version " + oldVersion + " to "
+				+ newVersion + ", which will destroy all old data");
+		db.execSQL("DROP TABLE IF EXISTS " + Constants.TABLE_HIS);
+		db.execSQL("DROP TABLE IF EXISTS " + Constants.TABLE_FAV);
+		onCreate(db);
+	}
+
+	public boolean PushDataHistory(String start_st_txt, String start_st_val,
+			String end_st_txt, String end_st_val,
+			String start_time_txt, String start_time_val,
+			String end_time_txt, String end_time_val) {
+		SQLiteDatabase db = null;
+		Cursor myCur = null;
+		long rowID = -1;
+		try {
+			db = this.getWritableDatabase();
+		} catch(Exception e) {
+			Log.e(Constants.LOG_TAG, "Error in getWritableDatabase" + e);
+		}
+		if(db == null) {
+			Log.e(Constants.LOG_TAG, "Cannot open writable DB");
+			return false;
+		}
+
+		ContentValues keyValPairs = new ContentValues(8);
+		keyValPairs.put(Constants.COL_START_STATION_TXT, start_st_txt);
+		keyValPairs.put(Constants.COL_START_STATION_VAL, start_st_val);
+		keyValPairs.put(Constants.COL_END_STATION_TXT, end_st_txt);
+		keyValPairs.put(Constants.COL_END_STATION_VAL, end_st_val);
+		keyValPairs.put(Constants.COL_START_TIME_TXT, start_time_txt);
+		keyValPairs.put(Constants.COL_START_TIME_VAL, start_time_val);
+		keyValPairs.put(Constants.COL_END_TIME_TXT, end_time_txt);
+		keyValPairs.put(Constants.COL_END_TIME_VAL, end_time_val);
+		db.beginTransaction();
+		try {
+			if(db.insert(Constants.TABLE_HIS, null, keyValPairs) < 0) {
+				Log.e(Constants.LOG_TAG, "Error writing to DB");
+				db.close();
+				return false;
+			}
+		} catch(Exception e) {
+			Log.e(Constants.LOG_TAG, "Error pushing data to DB " + e);
+			db.endTransaction();
+			db.close();
+			return false;
+		}
+		db.setTransactionSuccessful();
+		db.endTransaction();
+		try {
+			myCur = db.query(Constants.TABLE_HIS, new String [] {"_ID"}, null, null, null, null, null);
+			if(myCur == null) {
+				Log.e(Constants.LOG_TAG, "Select operation failed");
+				db.close();
+				return false;
+			}
+		} catch(Exception e) {
+			Log.e(Constants.LOG_TAG, "Error in db.query " + e);
+			db.close();
+			return false;
+		}
+		db.beginTransaction();
+		myCur.moveToFirst();
+		for(int i = myCur.getCount(); i > Constants.MAX_HIS_COUNT; i--) {
+			try {
+				rowID = myCur.getLong(0);
+				if(rowID < 0) {
+					db.close();
+					myCur.close();
+					Log.e(Constants.LOG_TAG, "Errornous row ID value= " + rowID);
+					return false;
+				}
+				int delCount = db.delete(Constants.TABLE_HIS, "_ID = " + rowID, null);
+				if(delCount <= 0) {
+					Log.w(Constants.LOG_TAG, "Extra row not deleted...");
+				}
+			} catch(Exception e) {
+				db.endTransaction();
+				myCur.close();
+				db.close();
+				Log.e(Constants.LOG_TAG, "Error Deleting a row " + e);
+				return false;
+			}
+			myCur.moveToNext();
+		}
+		db.setTransactionSuccessful();
+		db.endTransaction();
+		myCur.close();
+		db.close();
+		return true;
+	}
+
+	public ParameterSet [] GetHistory() {
+		ParameterSet [] paramsList = null;
+		paramsList = null;
+		SQLiteDatabase db = null;
+		Cursor myCur = null;
+		try {
+			db = this.getWritableDatabase();
+		} catch(Exception e) {
+			Log.e(Constants.LOG_TAG, "Error in getWritableDatabase" + e);
+		}
+		if(db == null) {
+			Log.e(Constants.LOG_TAG, "Cannot open writable DB");
+			return null;
+		}
+		try {
+			myCur = db.query(Constants.TABLE_HIS, new String [] {"*"}, null, null, null, null, null);
+			if(myCur == null) {
+				Log.e(Constants.LOG_TAG, "Select operation failed");
+				db.close();
+				return null;
+			}
+		} catch(Exception e) {
+			Log.e(Constants.LOG_TAG, "Error in db.query " + e);
+			db.close();
+			return null;
+		}
+		paramsList = new ParameterSet[myCur.getCount()];
+		myCur.moveToLast();
+		for(int i = 0; i < myCur.getCount(); i++) {
+			paramsList[i] = new ParameterSet();
+			paramsList[i].start_station_txt =  CommonUtilities.ToTitleCase(myCur.getString(1));
+			paramsList[i].start_station_val = myCur.getString(2);
+			paramsList[i].end_station_txt =  CommonUtilities.ToTitleCase(myCur.getString(3));
+			paramsList[i].end_station_val = myCur.getString(4);
+			paramsList[i].start_time_txt = myCur.getString(5);
+			paramsList[i].start_time_val = myCur.getString(6);
+			paramsList[i].end_time_txt = myCur.getString(7);
+			paramsList[i].end_time_val = myCur.getString(8);
+			myCur.moveToPrevious();
+		}
+		myCur.close();
+		db.close();
+		return paramsList;
+	}
+
+	public void ClearHistoryTable() {
+		SQLiteDatabase db = null;
+		try {
+			db = this.getWritableDatabase();
+		} catch(Exception e) {
+			Log.e(Constants.LOG_TAG, "Error in getWritableDatabase" + e);
+			return;
+		}
+		if(db == null) {
+			Log.e(Constants.LOG_TAG, "Cannot open writable DB");
+			return;
+		}
+		db.beginTransaction();
+		try {
+			db.delete(Constants.TABLE_HIS, null, null);
+		} catch(Exception e) {
+			Log.e(Constants.LOG_TAG, "Error in deleting all records in table" + e);
+			db.endTransaction();
+			db.close();
+			return;
+		}
+		db.setTransactionSuccessful();
+		db.endTransaction();
+		db.close();
+	}
+
+	public boolean PushDataFavourites(String start_st_txt, String start_st_val,
+			String end_st_txt, String end_st_val,
+			String start_time_txt, String start_time_val,
+			String end_time_txt, String end_time_val,
+			String name, Handler handler ) {
+		SQLiteDatabase db = null;
+		Cursor myCur = null;
+		int rowCount = Constants.MAX_FAV_COUNT;
+		Message myMsg = new Message();
+		String msgStr = "Error... Restart application";
+		myMsg.obj = (Object)msgStr;
+		myMsg.arg1 = Constants.THREAD_PUSH_DATA_FAVOURITES;
+		try {
+			db = this.getWritableDatabase();
+		} catch(Exception e) {
+			Log.e(Constants.LOG_TAG, "Error in getWritableDatabase" + e);
+			handler.sendMessage(myMsg);
+			return false;
+		}
+		if(db == null) {
+			Log.e(Constants.LOG_TAG, "Cannot open writable DB");
+			handler.sendMessage(myMsg);
+			return false;
+		}
+		try {
+			myCur = db.query(Constants.TABLE_FAV, new String [] {"_ID"}, null, null, null, null, null);
+			if(myCur == null) {
+				Log.e(Constants.LOG_TAG, "Select operation failed");
+				db.close();
+				handler.sendMessage(myMsg);
+				return false;
+			}
+		} catch(Exception e) {
+			Log.e(Constants.LOG_TAG, "Error in db.query " + e);
+			handler.sendMessage(myMsg);
+			return false;
+		}
+		rowCount = myCur.getCount();
+		myCur.close();
+		/**
+		 * If items in history table is less than the max count add data. Else return false.
+		 */
+		if(rowCount < Constants.MAX_FAV_COUNT) {
+			ContentValues keyValPairs = new ContentValues(9);
+			keyValPairs.put(Constants.COL_START_STATION_TXT, start_st_txt);
+			keyValPairs.put(Constants.COL_START_STATION_VAL, start_st_val);
+			keyValPairs.put(Constants.COL_END_STATION_TXT, end_st_txt);
+			keyValPairs.put(Constants.COL_END_STATION_VAL, end_st_val);
+			keyValPairs.put(Constants.COL_START_TIME_TXT, start_time_txt);
+			keyValPairs.put(Constants.COL_START_TIME_VAL, start_time_val);
+			keyValPairs.put(Constants.COL_END_TIME_TXT, end_time_txt);
+			keyValPairs.put(Constants.COL_END_TIME_VAL, end_time_val);
+			keyValPairs.put(Constants.COL_FAV_NAME, name);
+			db.beginTransaction();
+			try {
+				if( db.insert(Constants.TABLE_FAV, null, keyValPairs) < 0 ) {
+					Log.e(Constants.LOG_TAG, "Error writing to DB");
+					db.close();
+					handler.sendMessage(myMsg);
+					return false;
+				}
+			} catch(Exception e) {
+				Log.e(Constants.LOG_TAG, "Error pushing data to DB " + e);
+				db.endTransaction();
+				db.close();
+				handler.sendMessage(myMsg);
+				return false;
+			}
+			db.setTransactionSuccessful();
+			db.endTransaction();
+		}
+		else {
+			/** Maximum favourites limit exceeded **/
+			Log.w(Constants.LOG_TAG, "Maximum favourites limit exceeded");
+			db.close();
+			msgStr = "Maximum favourites limit of " + Constants.MAX_FAV_COUNT + " reached.";
+			myMsg.obj = (Object)msgStr;
+			handler.sendMessage(myMsg);
+			return false;
+		}
+		db.close();
+		msgStr = "Sucessfully added to favourites.";
+		myMsg.obj = (Object)msgStr;
+		handler.sendMessage(myMsg);
+		return true;
+	}
+
+	public ParameterSet [] GetFavourites() {
+		ParameterSet [] paramsList = null;
+		SQLiteDatabase db = null;
+		Cursor myCur = null;
+		try {
+			db = this.getWritableDatabase();
+		} catch(Exception e) {
+			Log.e(Constants.LOG_TAG, "Error in getWritableDatabase" + e);
+		}
+		if(db == null) {
+			Log.e(Constants.LOG_TAG, "Cannot open writable DB");
+			return null;
+		}
+		try {
+			myCur = db.query(Constants.TABLE_FAV, new String [] {"*"}, null, null, null, null, null);
+			if(myCur == null) {
+				Log.e(Constants.LOG_TAG, "Select operation failed");
+				db.close();
+				return null;
+			}
+		} catch(Exception e) {
+			Log.e(Constants.LOG_TAG, "Error in db.query " + e);
+			db.close();
+			return null;
+		}
+		paramsList = new ParameterSet[myCur.getCount()];
+		myCur.moveToFirst();
+		for(int i = 0; i < myCur.getCount(); i++) {
+			paramsList[i] = new ParameterSet();
+			paramsList[i].id = myCur.getLong(0);
+			paramsList[i].start_station_txt =  CommonUtilities.ToTitleCase(myCur.getString(1));
+			paramsList[i].start_station_val = myCur.getString(2);
+			paramsList[i].end_station_txt =  CommonUtilities.ToTitleCase(myCur.getString(3));
+			paramsList[i].end_station_val = myCur.getString(4);
+			paramsList[i].start_time_txt = myCur.getString(5);
+			paramsList[i].start_time_val = myCur.getString(6);
+			paramsList[i].end_time_txt = myCur.getString(7);
+			paramsList[i].end_time_val = myCur.getString(8);
+			paramsList[i].name = myCur.getString(9);
+			myCur.moveToNext();
+		}
+		myCur.close();
+		db.close();
+		return paramsList;
+	}
+
+	public void ClearFavouritesTable() {
+		SQLiteDatabase db = null;
+		try {
+			db = this.getWritableDatabase();
+		} catch(Exception e) {
+			Log.e(Constants.LOG_TAG, "Error in getWritableDatabase" + e);
+			return;
+		}
+		if(db == null) {
+			Log.e(Constants.LOG_TAG, "Cannot open writable DB");
+			return;
+		}
+		db.beginTransaction();
+		try {
+			db.delete(Constants.TABLE_FAV, null, null);
+		} catch(Exception e) {
+			Log.e(Constants.LOG_TAG, "Error in deleting all records in table" + e);
+			db.endTransaction();
+			db.close();
+			return;
+		}
+		db.setTransactionSuccessful();
+		db.endTransaction();
+		db.close();
+	}
+	public boolean DeleteFavRecord(long id) {
+		SQLiteDatabase db = null;
+		try {
+			db = this.getWritableDatabase();
+		} catch(Exception e) {
+			Log.e(Constants.LOG_TAG, "Error in getWritableDatabase" + e);
+			return false;
+		}
+		if(db == null) {
+			Log.e(Constants.LOG_TAG, "Cannot open writable DB");
+			return false;
+		}
+		db.beginTransaction();
+		try {
+			db.delete(Constants.TABLE_FAV, "_ID=" + String.valueOf(id), null);
+		} catch(Exception e) {
+			Log.e(Constants.LOG_TAG, "Error in deleting record" + e);
+			db.endTransaction();
+			db.close();
+			return false;
+		}
+		db.setTransactionSuccessful();
+		db.endTransaction();
+		db.close();
+		return true;
+	}
+
+	public boolean RenameFavRecord(long id, String newName) {
+		SQLiteDatabase db = null;
+		try {
+			db = this.getWritableDatabase();
+		} catch(Exception e) {
+			Log.e(Constants.LOG_TAG, "Error in getWritableDatabase" + e);
+			return false;
+		}
+		if(db == null) {
+			Log.e(Constants.LOG_TAG, "Cannot open writable DB");
+			return false;
+		}
+		db.beginTransaction();
+		try {
+			ContentValues keyValPairs = new ContentValues(1);
+			keyValPairs.put(Constants.COL_FAV_NAME, newName);
+			db.update(Constants.TABLE_FAV, keyValPairs, "_ID=" + String.valueOf(id), null);
+		} catch(Exception e) {
+			Log.e(Constants.LOG_TAG, "Error in deleting record" + e);
+			db.endTransaction();
+			db.close();
+			return false;
+		}
+		db.setTransactionSuccessful();
+		db.endTransaction();
+		db.close();
+		return true;
+	}
+}
---
+++ tags/Release_110/src/com/aselalee/trainschedule/FavouritesActivity.java
@@ -0,0 +1,238 @@
+/**
+* @copyright	Copyright (C) 2011 Asela Leelaratne
+* @license		GNU/GPL Version 3
+* 
+* This Application is released to the public under the GNU General Public License.
+* 
+* GNU/GPL V3 Extract.
+* 15. Disclaimer of Warranty.
+* THERE IS NO WARRANTY FOR THE PROGRAM, TO THE EXTENT PERMITTED BY APPLICABLE LAW.
+* EXCEPT WHEN OTHERWISE STATED IN WRITING THE COPYRIGHT HOLDERS AND/OR OTHER PARTIES
+* PROVIDE THE PROGRAM AS IS WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED,
+* INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+* FOR A PARTICULAR PURPOSE. THE ENTIRE RISK AS TO THE QUALITY AND PERFORMANCE OF THE
+* PROGRAM IS WITH YOU. SHOULD THE PROGRAM PROVE DEFECTIVE, YOU ASSUME THE COST OF ALL
+* NECESSARY SERVICING, REPAIR OR CORRECTION.
+*/
+
+package com.aselalee.trainschedule;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.ListActivity;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.ContextMenu;
+import android.view.ContextMenu.ContextMenuInfo;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.AdapterContextMenuInfo;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class FavouritesActivity extends ListActivity {
+	private ParameterSet [] paramsList = null;
+	private HisAndFavAdapter adapter = null;
+	private int postionToRename = 0;
+	private AnalyticsWrapper tracker;
+
+	private static final int DIALOG_RENAME_FAV = 5;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		/**
+		 * Setup analytics.
+		 */
+		tracker = new AnalyticsWrapper(FavouritesActivity.this);
+		tracker.TrackPageView("/FavouritesActivity");
+
+		ListView lv = getListView();
+		registerForContextMenu (lv);
+		lv.setOnItemClickListener(new OnItemClickListener() {
+			public void onItemClick(AdapterView<?> adv, View view,
+					int position, long id) {
+				if(paramsList != null) {
+					tracker.TrackEvent("FavouritesAcitiviy", "Get_Results", "List_Item_Click", 1);
+					getResults(paramsList[position]);
+				} else {
+					Log.w(Constants.LOG_TAG, "Favourites list is empty");
+				}
+			}
+		});
+		adapter = new HisAndFavAdapter(FavouritesActivity.this, paramsList, false);
+		setListAdapter(adapter);
+	}
+
+	@Override
+	public void onPause() {
+		super.onPause();
+		tracker.Dispatch();
+	}
+
+	@Override
+	public void onResume() {
+		super.onResume();
+		updateFavList();
+	}
+
+	@Override
+	public void onDestroy() {
+		super.onStop();
+		tracker.StopSession();
+	}
+
+	@Override
+	public void onConfigurationChanged(Configuration newConfig) {
+		super.onConfigurationChanged(newConfig);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.favourites_activity_menu, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+			case R.id.fav_menu_clear_fav:
+				DBDataAccess myDBAcc = new DBDataAccess(FavouritesActivity.this);
+				myDBAcc.ClearFavouritesTable();
+				myDBAcc.close();
+				updateFavList();
+				tracker.TrackEvent("FavouritesAcitiviy", "Clear_Favs", "Menu_Click", 1);
+				return true;
+			default:
+				return super.onOptionsItemSelected(item);
+		}
+	}
+
+	@Override
+	public void onCreateContextMenu(ContextMenu menu, View v,
+			ContextMenuInfo menuInfo) {
+		super.onCreateContextMenu(menu, v, menuInfo);
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.favourites_activity_context_menu, menu);
+		AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo)menuInfo;
+		menu.setHeaderTitle(paramsList[info.position].name);
+	}
+
+	@Override
+	public boolean onContextItemSelected(MenuItem item) {
+		AdapterContextMenuInfo info = (AdapterContextMenuInfo) item.getMenuInfo();
+		switch (item.getItemId()) {
+			case R.id.fav_ctx_menu_rename_fav:
+				postionToRename = info.position;
+				tracker.TrackEvent("FavouritesAcitiviy", "Rename_Fav", "Ctx_Menu_Click", 1);
+				showDialog(DIALOG_RENAME_FAV);
+				break;
+			case R.id.fav_ctx_menu_delete_fav:
+				DBDataAccess myDBAcc = new DBDataAccess(FavouritesActivity.this);
+				myDBAcc.DeleteFavRecord(paramsList[info.position].id);
+				myDBAcc.close();
+				tracker.TrackEvent("FavouritesAcitiviy", "Delete_Fav", "Ctx_Menu_Click", 1);
+				break;
+			default:
+				return super.onContextItemSelected(item);
+		}
+		updateFavList();
+		return true;
+	}
+
+	protected Dialog onCreateDialog(int id) {
+		Dialog dialog = null;
+		switch(id) {
+			case DIALOG_RENAME_FAV:
+				dialog = renameFavItem();
+				break;
+			default:
+				dialog = null;
+		}
+		return dialog;
+	}
+	protected void onPrepareDialog(int id, Dialog dialog) {
+		switch(id) {
+			case DIALOG_RENAME_FAV:
+				EditText et = (EditText)dialog.findViewById(R.id.dialog_new_name);
+				et.setText(paramsList[postionToRename].name);
+				et.setSelection(et.getText().length());
+				break;
+			default:
+				return;
+		}
+		return;
+	}
+
+	private void getResults(ParameterSet paramSet) {
+		Intent intent = CommonUtilities.GetResultViewIntent(FavouritesActivity.this);
+		CommonUtilities.PupulateIntentForResultsActivity(
+				paramSet.start_station_val, paramSet.start_station_txt,
+				paramSet.end_station_val, paramSet.end_station_txt,
+				paramSet.start_time_val, paramSet.start_time_txt,
+				paramSet.end_time_val, paramSet.end_time_txt,
+				intent);
+		startActivity(intent);
+	}
+
+	private Dialog renameFavItem() {
+		final Context thisContext = FavouritesActivity.this;
+		LayoutInflater factory = LayoutInflater.from(FavouritesActivity.this);
+		View textEntryView = factory.inflate(R.layout.text_entry_dialog, null);
+		final EditText et = (EditText)textEntryView.findViewById(R.id.dialog_new_name);
+		final CheckBox cb = (CheckBox)textEntryView.findViewById(R.id.dialog_isTimeFilterOnCB);
+		cb.setVisibility(View.GONE);
+		AlertDialog.Builder builder = new AlertDialog.Builder(FavouritesActivity.this);
+		builder.setView(textEntryView);
+		builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+			public void onClick(DialogInterface dialog, int id) {
+				String newName = "";
+				newName = et.getEditableText().toString();
+				if(newName.length() != 0) {
+					DBDataAccess myDBAcc = new DBDataAccess(thisContext);
+					myDBAcc.RenameFavRecord(paramsList[postionToRename].id, newName);
+					myDBAcc.close();
+				} else {
+					Toast.makeText(thisContext, "Invalid name.", Toast.LENGTH_LONG).show();
+				}
+				CommonUtilities.HideSoftKeyboard(et, getBaseContext());
+				updateFavList();
+			}
+		});
+		builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+			public void onClick(DialogInterface dialog, int id) {
+				CommonUtilities.HideSoftKeyboard(et, getBaseContext());
+				dialog.cancel();
+			}
+		});
+		builder.setTitle("Enter New Name");
+		AlertDialog alert = builder.create();
+		return alert;
+	}
+
+	private void updateFavList() {
+		/**
+		 * 1. Get favourites from database.
+		 * 2. Then show it in the list view.
+		 * 3. Call this when ever the database is updated.
+		 */
+		DBDataAccess myDBAcc = new DBDataAccess(FavouritesActivity.this);
+		paramsList = myDBAcc.GetFavourites();
+		adapter.paramSet = paramsList;
+		myDBAcc.close();
+		adapter.notifyDataSetChanged();
+		return;
+	}
+}
