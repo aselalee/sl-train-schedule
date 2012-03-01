d4eaae451b1fb604df030f3fbb5a997105ad481e
aselalee <aselalee@93d08951-fc07-b37f-cc7d-7371ff8aefea>
2012-03-01 03:58:53 +0000

    Released to market.

---
+++ tags/Release_111/src/com/aselalee/trainschedule/CommonUtilities.java
@@ -0,0 +1,361 @@
+/**
+* @copyright	Copyright (C) 2011 Asela Leelaratne
+* @license		GNU/GPL Version 3
+* 
+* This Application is released to the public under the GNU General Public License.
+* 
+* GNU/GPL V3 Extract.
+* 15. Disclaimer of Warranty.
+* THERE IS NO WARRANTY FOR THE PROGRAM, TO THE EXTENT PERMITTED BY APPLICABLE LAW.
+* EXCEPT WHEN OTHERWISE STATED IN WRITING THE COPYRIGHT HOLDERS AND/OR OTHER PARTIES
+* PROVIDE THE PROGRAM AS IS WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED,
+* INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+* FOR A PARTICULAR PURPOSE. THE ENTIRE RISK AS TO THE QUALITY AND PERFORMANCE OF THE
+* PROGRAM IS WITH YOU. SHOULD THE PROGRAM PROVE DEFECTIVE, YOU ASSUME THE COST OF ALL
+* NECESSARY SERVICING, REPAIR OR CORRECTION.
+*/
+
+package com.aselalee.trainschedule;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.ResolveInfo;
+import android.os.Handler;
+import android.os.Message;
+import android.os.Parcelable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.CheckBox;
+import android.widget.EditText;
+
+public class CommonUtilities {
+
+	/**
+	 * *********************************************
+	 * Static methods Than can be accessed directly.
+	 * *********************************************
+	 */
+	public static final void PupulateIntentForResultsActivity(
+			String station_from_val, String station_from_txt,
+			String station_to_val, String station_to_txt,
+			String time_from_val, String time_from_txt,
+			String time_to_val, String time_to_txt,
+			Intent intent) {
+		String date_today = android.text.format.DateFormat.format("yyyy-MM-dd", new java.util.Date()).toString();
+		intent.putExtra("station_from", station_from_val);
+		intent.putExtra("station_from_txt", station_from_txt);
+		intent.putExtra("station_to", station_to_val);
+		intent.putExtra("station_to_txt",station_to_txt);
+		intent.putExtra("time_from", time_from_val);
+		intent.putExtra("time_from_txt", time_from_txt);
+		intent.putExtra("time_to", time_to_val);
+		intent.putExtra("time_to_txt", time_to_txt);
+		intent.putExtra("date_today", date_today);
+		return;
+	}
+
+	public static final void HideSoftKeyboard(View view, Context context) {
+		if(view == null || context == null) return;
+		InputMethodManager imm = (InputMethodManager)context.getSystemService(Context.INPUT_METHOD_SERVICE);
+		imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+	}
+
+	public static final String ToTitleCase(String string) {
+		String [] words = string.split(" ");
+		String result  = "";
+		for(int i = 0; i < words.length; i++) {
+			result += toInitialCap( words[i] ) + " ";
+		}
+		return result.trim();
+	}
+
+	private static final String toInitialCap(String word) {
+		if( word != null && word.length() > 1) {
+			return String.valueOf(word.toUpperCase().charAt(0)) + word.substring(1).toLowerCase();
+		}
+		return "";
+	}
+
+	/**
+	 * Match "from time" in spinner to actual string received by the server.
+	 */
+	public static String MapTimeFrom(int pos) {
+		String time_from[] = {"00:00:01","01:00:00","02:00:00","03:00:00","04:00:00","05:00:00",
+				"06:00:00","07:00:00","08:00:00","09:00:00","10:00:00","11:00:00",
+				"11:59:59","13:00:00","14:00:00","15:00:00","16:00:00","17:00:00",
+				"18:00:00","19:00:00","20:00:00","21:00:0,","22:00:00","23:00:00"};
+		if(pos == -1) {
+			return time_from[time_from.length - 1];
+		}
+		return time_from[pos];
+	}
+
+	/**
+	 * Match "to time" in spinner to actual string received by the server.
+	 */
+	public static String MapTimeTo(int pos) {
+		String time_to[] = {"01:00:00","02:00:00","03:00:00","04:00:00","05:00:00","06:00:00","07:00:00",
+				"08:00:00","09:00:00","10:00:00","11:00:00","11:59:59","13:00:00","14:00:00",
+				"15:00:00","16:00:00","17:00:00","18:00:00","19:00:00","20:00:00","21:00:00",
+				"22:00:0,","23:00:00","23:59:59"};
+		if(pos == -1) {
+			return time_to[time_to.length - 1];
+		}
+		return time_to[pos];
+	}
+
+	public static void AddParamsToHistory(final Context mPackageContext,
+			final String mStationFromTxt, final String mStationFromVal,
+			final String mStationToTxt, final String mStationToVal,
+			final String mTimeFromTxt, final String mTimeFromVal,
+			final String mTimeToTxt, final String mTimeToVal) {
+		Runnable runnable = new Runnable() {
+			public void run() {
+				DBDataAccess myDBAcc = new DBDataAccess(mPackageContext);
+				myDBAcc.PushDataHistory(mStationFromTxt, mStationFromVal,
+						mStationToTxt, mStationToVal,
+						mTimeFromTxt, mTimeFromVal,
+						mTimeToTxt, mTimeToVal);
+				myDBAcc.close();
+			}
+		};
+		Thread thread = new Thread(runnable);
+		thread.start();
+		return;
+	}
+
+	public static void ShareApplication(Context mContext) {
+		Intent intent = new Intent(android.content.Intent.ACTION_SEND);
+		intent.setType("text/plain");
+		intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+		intent.putExtra(Intent.EXTRA_SUBJECT, "Search Sri Lanka Railway Time Table");
+		intent.putExtra(Intent.EXTRA_TITLE, "Search Sri Lanka Railway Time Table");
+		intent.putExtra(Intent.EXTRA_TEXT, "Search \"Sri Lanka Railway Time Table\" on your Android. http://market.android.com/details?id=com.aselalee.trainschedule");
+		mContext.startActivity(Intent.createChooser(intent, "Spread the word"));
+		return;
+	}
+
+	/**
+	 * Intent chooser is customized to remove unwanted apps.
+	 * 1. FaceBook has bug where only links can be shared.
+	 * 2. Cannot share this type of content via Google Docs and Skype.
+	 */
+	public static void ShareResult(Context mContext, String mResult, String mTitle) {
+		List<Intent> targetedShareIntents = new ArrayList<Intent>();
+		Intent shareIntent = new Intent(android.content.Intent.ACTION_SEND);
+		shareIntent.setType("text/plain");
+		List<ResolveInfo> resInfo = mContext.getPackageManager().queryIntentActivities(shareIntent, 0);
+		if(!resInfo.isEmpty()) {
+			for(ResolveInfo resolveInfo : resInfo) {
+				String packageName = resolveInfo.activityInfo.packageName;
+				Intent targetedShareIntent = new Intent(android.content.Intent.ACTION_SEND);
+				targetedShareIntent.setType("text/plain");
+				targetedShareIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, mTitle);
+				targetedShareIntent.putExtra(android.content.Intent.EXTRA_TITLE, mTitle);
+				targetedShareIntent.putExtra(android.content.Intent.EXTRA_TEXT, mResult);
+				if(!packageName.toLowerCase().contains("com.facebook.katana") &&
+					!packageName.toLowerCase().contains("com.google.android.apps.docs") &&
+					!packageName.toLowerCase().contains("com.skype.raider")) {
+					targetedShareIntent.setPackage(packageName);
+					targetedShareIntents.add(targetedShareIntent);
+				}
+			}
+			Intent chooserIntent = Intent.createChooser(targetedShareIntents.remove(0), "Send your result");
+			chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, targetedShareIntents.toArray(new Parcelable[]{}));
+			mContext.startActivity(chooserIntent);
+		}
+		return;
+	}
+
+	public static String FormatResultForSharing(Result results[]) {
+		int count = results.length;
+		String outputStr = "";
+		String tmp;
+		if(count < 0) return outputStr;
+		outputStr += "From: ";
+		outputStr += results[0].startStationName;
+		outputStr += "\n";
+		outputStr += "To: ";
+		outputStr += results[0].endStationName;
+		outputStr += "\n";
+		for(int index = 0; index < count; index++) {
+			tmp = "(" + String.valueOf(index + 1) + ")\n";
+			outputStr += tmp;
+			outputStr += "Depatrue: ";
+			outputStr += results[index].depatureTime_str;
+			outputStr += " - ";
+			outputStr += "Arrival: ";
+			outputStr += results[index].arrivalAtDestinationTime_str;
+			outputStr += "\n";
+			outputStr += "Frequency: ";
+			outputStr += results[index].fDescription_original;
+			outputStr += "\n";
+			outputStr += "Duration: ";
+			outputStr += results[index].duration_str;
+			outputStr += "\n";
+		}
+		return outputStr;
+	}
+
+	public static void NewVersionInfo(Context mContext) {
+		AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
+		builder.setMessage(R.string.version_info)
+		       .setCancelable(false)
+		       .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+		           public void onClick(DialogInterface dialog, int id) {
+		                dialog.cancel();
+		           }
+		       });
+		AlertDialog alert = builder.create();
+		alert.show();
+		return;
+	}
+
+	/**
+	 * **********************************************************************
+	 * Utilities methods that can be accessed via an instance of this class.
+	 * **********************************************************************
+	 */
+	private Context packageContext = null;
+	private Handler handler = null;
+	private String station_from_txt = "";
+	private String station_from_val = "";
+	private String station_to_txt = "";
+	private String station_to_val = "";
+	private String time_from_txt = "";
+	private String time_from_val = "";
+	private String time_to_txt = "";
+	private String time_to_val = "";
+
+	public CommonUtilities(Context mPackageContext) {
+		packageContext = mPackageContext;
+	}
+
+	/**
+	 * Add data to favourites table.
+	 */
+	public AlertDialog GetNewFavNameAndAddToFavs(final boolean isCheckBoxGone,
+			String mStationFromTxt, String mStationFromVal,
+			String mStationToTxt, String mStationToVal,
+			String mTimeFromTxt, String mTimeFromVal,
+			String mTimeToTxt, String mTimeToVal, Handler mHandler) {
+		/**
+		 * Init Class variables.
+		 */
+		handler = mHandler;
+		updateClassVariables(mStationFromTxt, mStationFromVal,
+				mStationToTxt, mStationToVal,
+				mTimeFromTxt, mTimeFromVal,
+				mTimeToTxt, mTimeToVal);
+
+		LayoutInflater factory = LayoutInflater.from(packageContext);
+		View textEntryView = factory.inflate(R.layout.text_entry_dialog, null);
+		final EditText et = (EditText)textEntryView.findViewById(R.id.dialog_new_name);
+		final CheckBox cb = (CheckBox)textEntryView.findViewById(R.id.dialog_isTimeFilterOnCB);
+		if(isCheckBoxGone == true) {
+			cb.setVisibility(View.GONE);
+		}
+		et.setText(station_from_txt + " - " + station_to_txt);
+		et.setSelection(et.getText().length());
+		AlertDialog.Builder builder = new AlertDialog.Builder(packageContext);
+		builder.setView(textEntryView);
+		builder.setPositiveButton("Save", new DialogInterface.OnClickListener() {
+			public void onClick(DialogInterface dialog, int id) {
+				boolean isTimeFilterON = true;
+				if(isCheckBoxGone == false) {
+					isTimeFilterON = cb.isChecked();
+				}
+				addParamsToFavs(et.getEditableText().toString(), isTimeFilterON);
+				CommonUtilities.HideSoftKeyboard(et, packageContext);
+			}
+		});
+		builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+			public void onClick(DialogInterface dialog, int id) {
+				CommonUtilities.HideSoftKeyboard(et, packageContext);
+				dialog.cancel();
+			}
+		});
+		builder.setTitle("Enter New Name");
+		AlertDialog alert = builder.create();
+		return alert;
+	}
+
+	private void addParamsToFavs(final String name_txt, final boolean isTimeFilterON) {
+		if(name_txt.length() == 0) {
+			Message myMsg = new Message();
+			String msgStr = "Invalid name";
+			myMsg.obj = (Object)msgStr;
+			myMsg.arg1 = Constants.THREAD_PUSH_DATA_FAVOURITES;
+			handler.sendMessage(myMsg);
+			return;
+		}
+		Runnable runnable = new Runnable() {
+				public void run() {
+					DBDataAccess myDBAcc = new DBDataAccess(packageContext);
+					if(isTimeFilterON == true) {
+						myDBAcc.PushDataFavourites(station_from_txt, station_from_val,
+								station_to_txt, station_to_val,
+								time_from_txt, time_from_val,
+								time_to_txt, time_to_val,
+								name_txt, handler );
+					} else {
+						myDBAcc.PushDataFavourites(station_from_txt, station_from_val,
+								station_to_txt, station_to_val,
+								Constants.TIME_FIRST_FROM,
+								CommonUtilities.MapTimeFrom(0),
+								Constants.TIME_LAST_TO,
+								CommonUtilities.MapTimeTo(-1),
+								name_txt, handler );
+					}
+					myDBAcc.close();
+				}
+		};
+		Thread thread = new Thread(runnable);
+		thread.start();
+	}
+
+	public void UpdateFavParams(String mStationFromTxt, String mStationFromVal,
+				String mStationToTxt, String mStationToVal,
+				String mTimeFromTxt, String mTimeFromVal,
+				String mTimeToTxt, String mTimeToVal, Dialog dialog) {
+
+		/**
+		 * Update Class variables.
+		 */
+		updateClassVariables(mStationFromTxt, mStationFromVal,
+				mStationToTxt, mStationToVal,
+				mTimeFromTxt, mTimeFromVal,
+				mTimeToTxt, mTimeToVal);
+
+		/**
+		 * Update edit text box.
+		 */
+		EditText et = null;
+		et = (EditText)dialog.findViewById(R.id.dialog_new_name);
+		et.setText(station_from_txt + " - " + station_to_txt);
+		et.setSelection(et.getText().length());
+
+		return;
+	}
+	
+	private void updateClassVariables(String mStationFromTxt, String mStationFromVal,
+			String mStationToTxt, String mStationToVal,
+			String mTimeFromTxt, String mTimeFromVal,
+			String mTimeToTxt, String mTimeToVal) {
+		station_from_txt = mStationFromTxt;
+		station_from_val = mStationFromVal;
+		station_to_txt = mStationToTxt;
+		station_to_val = mStationToVal;
+		time_from_txt = mTimeFromTxt;
+		time_from_val = mTimeFromVal;
+		time_to_txt = mTimeToTxt;
+		time_to_val = mTimeToVal;
+	}
+}
---
+++ tags/Release_111/src/com/aselalee/trainschedule/GetResultsFromSite.java
@@ -0,0 +1,313 @@
+/**
+* @copyright	Copyright (C) 2011 Asela Leelaratne
+* @license		GNU/GPL Version 3
+* 
+* This Application is released to the public under the GNU General Public License.
+*
+* GNU/GPL V3 Extract. 
+* 15. Disclaimer of Warranty.
+* THERE IS NO WARRANTY FOR THE PROGRAM, TO THE EXTENT PERMITTED BY APPLICABLE LAW.
+* EXCEPT WHEN OTHERWISE STATED IN WRITING THE COPYRIGHT HOLDERS AND/OR OTHER PARTIES
+* PROVIDE THE PROGRAM AS IS WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED,
+* INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+* FOR A PARTICULAR PURPOSE. THE ENTIRE RISK AS TO THE QUALITY AND PERFORMANCE OF THE
+* PROGRAM IS WITH YOU. SHOULD THE PROGRAM PROVE DEFECTIVE, YOU ASSUME THE COST OF ALL
+* NECESSARY SERVICING, REPAIR OR CORRECTION.
+*/
+
+package com.aselalee.trainschedule;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.utils.URLEncodedUtils;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+
+public class GetResultsFromSite extends Thread {
+	private Handler mHandler = null;
+	private String station_from;
+	private String station_to;
+	private String time_from;
+	private String time_to;
+	private String date_today;
+	private volatile Result [] results = null;
+	private volatile int errorCode = Constants.ERR_NO_ERROR;
+
+	private String errorString = "No Error";
+
+	public GetResultsFromSite(Handler handle, String st_from, String st_to, String tm_from, String tm_to, String date) {
+		mHandler = handle;
+		station_from = st_from;
+		station_to = st_to;
+		time_from = tm_from;
+		time_to = tm_to;
+		date_today = date;
+	}
+
+	public void run() {
+		/**
+		 * Call the "GetResults" method to retrieve data from server.
+		 */ 
+		GetResultsViaJASON(station_from, station_to, time_from, time_to, date_today);
+		/**
+		 * This will send message to the calling thread to continue and display data.
+		 */
+		Message myMsg = new Message();
+		myMsg.arg1 = Constants.THREAD_GET_RESULTS;
+		mHandler.sendMessage(myMsg);
+	}
+
+	private void GetResultsViaJASON(String station_from, String station_to,
+			String time_from, String time_to, String date_today)
+	{
+		/**
+		 * Create name value pairs to be sent the the above URL.
+		 * variable names were extracted manually from the site.
+		 */
+		List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(7);
+		nameValuePairs.add(new BasicNameValuePair("lang", "en"));
+		nameValuePairs.add(new BasicNameValuePair("startStationCode", station_from));
+		nameValuePairs.add(new BasicNameValuePair("endStationCode", station_to));
+		nameValuePairs.add(new BasicNameValuePair("arrivalTime", time_from));
+		nameValuePairs.add(new BasicNameValuePair("depatureTime", time_to));
+		nameValuePairs.add(new BasicNameValuePair("currentDate", date_today));
+		nameValuePairs.add(new BasicNameValuePair("currentTime","01:00:00"));
+		String strParams =  URLEncodedUtils.format(nameValuePairs, "utf-8");
+		String url = Constants.JASONURL + "?" + strParams;
+
+		/**
+		 * Setup networking.
+		 * Then set HTTP POST data.
+		 */
+		HttpGet httpGet = new HttpGet(url);
+		HttpClient httpClient = new DefaultHttpClient();
+		HttpResponse response = null;
+
+		/**
+		 * Send HTTP POST request.
+		 */
+		try {
+			response = httpClient.execute(httpGet);
+		} catch(ClientProtocolException e) {
+			errorCode = Constants.ERR_NETWORK_ERROR;
+			errorString = "HTTPERROR : ClientProtocolException : " + e;
+			Log.e(Constants.LOG_TAG, errorString);
+			results = null;
+			return;
+		} catch(IOException e) {
+			errorCode = Constants.ERR_NETWORK_ERROR;
+			errorString = "HTTPERROR : IOException : " + e;
+			Log.e(Constants.LOG_TAG, errorString);
+			results = null;
+			return;
+		}
+
+		/**
+		 * Get output from response.
+		 */
+		InputStream ips = null;
+		try {
+			ips = response.getEntity().getContent();
+		} catch(IOException e) {
+			errorCode = Constants.ERR_ERROR;
+			errorString = "getEntity.getContentERROR : IOException : " + e;
+			Log.e(Constants.LOG_TAG, errorString);
+			results = null;
+			return;
+		} catch(IllegalStateException e) {
+			errorCode = Constants.ERR_ERROR;
+			errorString = "getEntity.getContentERROR : IllegalStateException : " + e;
+			Log.e(Constants.LOG_TAG, errorString);
+			results = null;
+			return;
+		}
+		/**
+		 * Read output result from server.
+		 */
+		StringBuilder strBuilder = new StringBuilder();
+		try {
+			char[] bytes = new char[1024];
+			int numRead = 0;
+			BufferedReader reader = new BufferedReader(new InputStreamReader(ips, "UTF-8"));
+			while((numRead = reader.read(bytes)) > 0) {
+				strBuilder.append(new String(bytes, 0, numRead));
+			}
+		} catch(IOException e) {
+			errorCode = Constants.ERR_ERROR;
+			errorString = "InputStreamReaderERROR : IOException - Read Error : " + e;
+			Log.e(Constants.LOG_TAG, errorString);
+			results = null;
+			return;
+		}
+		JSONToResultsList(strBuilder.toString());
+	}
+
+	private void JSONToResultsList(String strJSON) {
+		JSONObject jObject;
+		JSONArray trainsArray;
+		String strTmp;
+
+		try {
+			jObject = new JSONObject(strJSON); 
+		} catch(JSONException e) {
+			errorCode = Constants.ERR_JASON_ERROR;
+			errorString =  "JASONObjectERROR : Error Parsing JSON string : " + e;
+			Log.e(Constants.LOG_TAG, errorString);
+			results = null;
+			return;
+		}
+		try {
+			trainsArray = jObject.getJSONArray("trains");
+		} catch(JSONException e) {
+			errorCode = Constants.ERR_JASON_ERROR;
+			errorString = "getJASONArrayERROR : Error Parsing JSON object :" + e;
+			Log.e(Constants.LOG_TAG, errorString);
+			results = null;
+			return;
+		}
+		if(trainsArray.length() < 1) {
+			errorCode = Constants.ERR_NO_RESULTS_FOUND_ERROR;
+			errorString = "No Results Found";
+			results = null;
+			return;
+		}
+		results = new Result[trainsArray.length()];
+		for(int i = 0; i < trainsArray.length(); i++) {
+			SimpleDateFormat dateFormatterIn = new SimpleDateFormat("HH:mm:ss");
+			SimpleDateFormat dateFormatterOut = new SimpleDateFormat("HH:mm");
+			try {
+				results[i] = new Result();
+				results[i].name = trainsArray.getJSONObject(i).getString("name").toString().trim();
+				strTmp = trainsArray.getJSONObject(i).getString("arrivalTime").toString().trim();
+				results[i].arrivalTime_dt = dateFormatterIn.parse(strTmp);
+				results[i].arrivalTime_str = dateFormatterOut.format(results[i].arrivalTime_dt);
+				strTmp = trainsArray.getJSONObject(i).getString("depatureTime").toString().trim();
+				results[i].depatureTime_dt = dateFormatterIn.parse(strTmp);
+				results[i].depatureTime_str = dateFormatterOut.format(results[i].depatureTime_dt);
+				strTmp = trainsArray.getJSONObject(i).getString("arrivalAtDestinationTime").toString().trim();
+				results[i].arrivalAtDestinationTime_dt = dateFormatterIn.parse(strTmp);
+				results[i].arrivalAtDestinationTime_str = dateFormatterOut.format(results[i].arrivalAtDestinationTime_dt);
+				strTmp = trainsArray.getJSONObject(i).getString("delayTime").toString().trim();
+				results[i].delayTime_str = chop(strTmp);
+				results[i].comment = trainsArray.getJSONObject(i).getString("comment").toString().trim();
+				results[i].startStationName = CommonUtilities.ToTitleCase(
+						trainsArray.getJSONObject(i).getString("startStationName").toString().trim());
+				results[i].endStationName = CommonUtilities.ToTitleCase(
+						trainsArray.getJSONObject(i).getString("endStationName").toString().trim());
+				results[i].toTrStationName = CommonUtilities.ToTitleCase(
+						trainsArray.getJSONObject(i).getString("toTrStationName").toString().trim());
+				results[i].fDescription_original =
+						CommonUtilities.ToTitleCase(trainsArray.getJSONObject(i).getString("fDescription").toString().trim());
+				results[i].fDescription = formatFrequency(results[i].fDescription_original);
+				results[i].tyDescription = CommonUtilities.ToTitleCase(
+						trainsArray.getJSONObject(i).getString("tyDescription").toString().trim());
+				results[i].duration_str = calcDuration(results[i].depatureTime_dt,
+						results[i].arrivalAtDestinationTime_dt);
+			} catch(JSONException e) {
+				errorCode = Constants.ERR_JASON_ERROR;
+				errorString = "getJSONObject.getStringError : Error Parsing JSON array object : " + e;
+				Log.e(Constants.LOG_TAG, errorString);
+				results = null;
+				return;
+			} catch(ParseException e) {
+				errorCode = Constants.ERR_DATE_STRING_PARSE_ERROR;
+				errorString = "dateFormatter.parse() : Error Parsing Time String : " + e;
+				Log.e(Constants.LOG_TAG, errorString);
+				results = null;
+				return;
+			}
+		}
+	}
+
+	private String formatFrequency(String frequency) {
+		String result = frequency;
+		int freqLength = frequency.length();
+		if( freqLength > 12) {
+			int firstSpace = -1;
+			int secondSpace = -1;
+			firstSpace = frequency.indexOf(" ");
+			if(firstSpace > 0) {
+				secondSpace = frequency.indexOf(" ", firstSpace + 1);
+			}
+			if(secondSpace > 0) {
+				char [] charArray = new char[freqLength];
+				frequency.getChars(0, freqLength, charArray, 0);
+				charArray[secondSpace] = '\n';
+				result = null;
+				result = new String(charArray);
+			}
+		}
+		return result;
+	}
+
+	private String calcDuration(Date depatureTime, Date arrAtDestinationTime) {
+		String durationStr = "";
+		long startTimeInMilliSecs = depatureTime.getTime();
+		long endTimeInMilliSecs = arrAtDestinationTime.getTime();
+		long durationInMilliSecs = endTimeInMilliSecs - startTimeInMilliSecs;
+		if(durationInMilliSecs > 0) {
+			int hours = (int)(durationInMilliSecs/1000/60/60);
+			int mins = (int)(durationInMilliSecs/1000/60)%60;
+			durationStr = strToDoubleDigits(hours) + ":" + strToDoubleDigits(mins);
+		} else {
+			durationStr = "---/---";
+		}
+		return durationStr;
+	}
+
+	private String strToDoubleDigits(int value) {
+		String output = "";
+		if(value <= 9) {
+			output += "0" + String.valueOf(value);
+		} else {
+			output += String.valueOf(value);
+		}
+		return output;
+	}
+
+	private String chop(String strIn) {
+		String strOut;
+		if(strIn == null) {
+			return "";
+			}
+		int strLen = strIn.length();
+		if(strLen < 4) {
+			return "";
+		}
+		int lastIdx = strLen - 3;
+		strOut = strIn.substring(0, lastIdx);
+		return strOut;
+	}
+
+	public int GetErrorCode() {
+		return errorCode;
+	}
+
+	public String GetErrorString() {
+		return errorString;
+	}
+	
+	public Result [] GetResults() {
+		return results;
+	}
+}
